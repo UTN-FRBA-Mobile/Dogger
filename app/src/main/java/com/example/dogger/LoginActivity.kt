6dc3f58250b2e91aa8e6332f6e462be231eb6458
joncolque <joncolque@gmail.com>
2019-12-01 21:38:56 -0300

    Se agregan imagenes UI

--- app/src/main/java/com/example/dogger/LoginActivity.kt
+++ app/src/main/java/com/example/dogger/LoginActivity.kt
@@ -22,10 +22,12 @@ import androidx.core.app.ComponentActivity
 import androidx.core.app.ComponentActivity.ExtraData
 import androidx.core.content.ContextCompat.getSystemService
 import android.icu.lang.UCharacter.GraphemeClusterBreak.T
-
+import com.google.firebase.firestore.FirebaseFirestore
 
 
 class LoginActivity : AppCompatActivity() {
+
+    val db = FirebaseFirestore.getInstance()
 
     private lateinit var txtUser: EditText
     private lateinit var txtPassword: EditText
@@ -55,6 +57,8 @@ class LoginActivity : AppCompatActivity() {
             }
         }, intentFilter)
         //** snip **//
+
+        //initialData() //Solo correr sino se tienen datos en Firestore.
     }
 
     fun forgotPassword(view:View){
@@ -114,6 +118,119 @@ class LoginActivity : AppCompatActivity() {
             else -> { // Note the block
                 Toast.makeText(this,"Error:" + usertype, Toast.LENGTH_LONG).show()
             }
+        }
+    }
+
+    fun initialData(){
+
+        val mascotas = arrayListOf(
+            Mascota(
+                1,
+                "Puchi",
+                R.drawable.dog1,
+                -34.920345,	-57.969559,
+                "Bill",
+                R.drawable.owner1,
+                "+549 11 6572 4625",
+                1
+            ),
+            Mascota(
+                2,
+                "Firulais",
+                R.drawable.dog2,
+                -32.950001,	-60.666668,
+                "Einstein",
+                R.drawable.owner2,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                3,
+                "Moncho",
+                R.drawable.dog3,
+                -31.416668,	-64.183334,
+                "Mark",
+                R.drawable.owner3,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                4,
+                "Anga",
+                R.drawable.dog4,
+                -32.888355,	-68.838844,
+                "Carl",
+                R.drawable.owner4,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                5,
+                "Canuto",
+                R.drawable.dog5,
+                -24.184340,	-65.302177,
+                "Rene",
+                R.drawable.owner5,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                6,
+                "Boby",
+                R.drawable.dog6,
+                -37.320480,	-59.132904,
+                "Facundo",
+                R.drawable.owner6,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                7,
+                "Trapo",
+                R.drawable.dog7,
+                -35.101933,	-60.438538,
+                "Steve",
+                R.drawable.owner7,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                8,
+                "Mugroso",
+                R.drawable.dog8,
+                -41.134258,	-71.308525,
+                "Tony",
+                R.drawable.owner8,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                9,
+                "Pulgoso",
+                R.drawable.dog9,
+                -26.808285,	-65.217590,
+                "Stephen",
+                R.drawable.owner9,
+                "+549 11 61009016",
+                1
+            ),
+            Mascota(
+                10,
+                "Manaos",
+                R.drawable.dog10,
+                -34.796581,	-58.276012,
+                "Elon",
+                R.drawable.owner10,
+                "+549 11 61009016",
+                1
+            )
+        )
+
+        mascotas.forEach { mascota ->
+            db.collection("mascotas")
+                .add(mascota)
+                .addOnSuccessListener { documentReference -> Log.d("[NEWDOCUMENT]", "DocumentSnapshot added with ID: ${documentReference.id}") }
+                .addOnFailureListener { e -> Log.w("[NEWDOCUMENT-ERROR]", "Error adding document", e) }
         }
     }
 }
